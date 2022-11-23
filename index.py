474451e13236bbb6fb17d852dac58341dc4e9f69
Lucas De Lavra Pinto <lucasdelavrapinto@gmail.com>
2022-11-23 15:50:43 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,39 @@
+from pytube import YouTube
+from prompter import yesno
+
+import os
+pathDownload = './downloads';
+res = True
+
+links_baixados = [];
+
+def set_global_value(value):
+    global res;
+    res = value;
+
+def downloadYouTube(videourl, path):
+
+    if(videourl not in links_baixados):
+        yt = YouTube(videourl)
+        yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+        if not os.path.exists(path):
+            os.makedirs(path)
+        yt.download(path)
+        links_baixados.append(videourl);
+        print('Download concluido com sucesso em: ' + pathDownload)
+    else:
+        print('Esse link já foi usado');
+    
+
+def execute():
+    videoLink = input ("Informe o link do vídeo: ")
+    print('Iniciando download do vídeo');
+    downloadYouTube(videoLink, pathDownload)
+    set_global_value(yesno('Deseja baixar mais algum vídeo?'));
+
+while res:
+    execute();
+else:
+    print(links_baixados)
+
+
