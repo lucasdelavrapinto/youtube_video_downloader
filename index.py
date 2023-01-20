bba9ca5261cb526ecb02eb4d6f3bf58e27c361d1
Lucas De Lavra Pinto <lucasdelavrapinto@gmail.com>
2023-01-20 08:31:34 -0300

    fix: simplificado download do video e atualizado para buscar 1080p

--- index.py
+++ index.py
@@ -4,38 +4,24 @@ from pytube.cli import on_progress
 
 import os
 pathDownload = './downloads';
-res = True
-
-links_baixados = [];
-
-def set_global_value(value):
-    global res;
-    res = value;
 
 def downloadYouTube(videourl, path):
-
-    if(videourl not in links_baixados):
-        yt = YouTube(videourl, on_progress_callback=on_progress)
-        # yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-        yt = yt.streams.get_highest_resolution()
-        if not os.path.exists(path):
-            os.makedirs(path)
-        yt.download(path)
-        links_baixados.append(videourl);
-        print('Download concluido com sucesso em: ' + pathDownload)
-    else:
-        print('Esse link já foi usado');
+    
+    yt = YouTube(videourl, on_progress_callback=on_progress)
+    # yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    # yt = yt.streams.get_highest_resolution()
+    yt = yt.streams.filter(res="1080p").first().download()
+    if not os.path.exists(path):
+        os.makedirs(path)
+    yt.download(path)
+    
+    print('Download concluido com sucesso em: ' + pathDownload)
+    
     
 
 def execute():
     videoLink = input ("Informe o link do vídeo: ")
-    print('Iniciando download do vídeo');
+    print('Iniciando download do vídeo')
     downloadYouTube(videoLink, pathDownload)
-    set_global_value(yesno('Deseja baixar mais algum vídeo?'));
 
-while res:
-    execute();
-else:
-    print(links_baixados)
-
-
+execute()
